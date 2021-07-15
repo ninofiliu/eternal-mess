7ffef0d3494756f9066bf3b6ecd0bd488296ded9
Nino Filiu <nino.filiu@gmail.com>
2021-07-15 17:13:50 +0200

    basic web setup

---
+++ snowpack.config.js
@@ -0,0 +1,15 @@
+/** @type {import('snowpack').SnowpackUserConfig} */
+module.exports = {
+  mount: {
+    public: {
+      url: '/',
+      static: true,
+    },
+    src: {
+      url: '/dist',
+    },
+  },
+  plugins: [
+    '@snowpack/plugin-typescript',
+  ],
+};
