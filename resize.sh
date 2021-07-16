9670375afa4f75c83e0482e2e6757f2c1e74ce72
Nino Filiu <nino.filiu@gmail.com>
2021-07-16 16:24:26 +0200

    add resize script

---
+++ resize.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]
+then
+  echo "usage: source resize.sh WIDTH HEIGHT"
+  return
+fi
+
+w=$1
+h=$2
+dir=in/${w}x${h}
+mkdir -p $dir
+
+for file in in/src/*
+do
+
+  base=$(basename $file)
+  out=$dir/$base
+  if [ -f $out ]
+  then
+    echo "$base: already resized, skipping..."
+    continue
+  fi
+
+  vw=$(ffprobe -v error -select_streams v:0 -show_entries stream=width -of csv=s=x:p=0 $file)
+  vh=$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of csv=s=x:p=0 $file)
+  if [ $(($vw / $vh)) -gt $(($w / $h )) ]
+  then
+    sw=$(($vh * $w / $h))
+    sh=$vh
+    sx=$((($vw - $sw) / 2))
+    sy=0
+  else
+    sw=$vw
+    sh=$(($vw * $h / $w))
+    sx=0
+    sy=$((($vh - $sh) / 2))
+  fi
+
+  echo "$base: cropping..."
+  ffmpeg -v error -i $file -filter:v "crop=$sw:$sh:$sx:$sy" $base
+  echo "$base: resizing..."
+  ffmpeg -v error -i $base -vf scale=$w:$h $out
+  rm $base
+
+done
+
+echo "done"
