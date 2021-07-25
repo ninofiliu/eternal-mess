fd1c117f18867e1ccc116d70d87bd886fbff61ec
Nino Filiu <nino.filiu@gmail.com>
2021-07-25 08:59:40 +0200

    flavor brouillard

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const sp = new URLSearchParams(window.location.search);
 const fn = sp.get('fn');
 
 if (fn === 'run') {
-  run(flavors.clemence);
+  run(flavors.brouillard);
 }
 if (fn === 'prepare') {
   const w = +sp.get('w');
